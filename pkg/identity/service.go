ef7c0b71622d17ef6d22f5209d05b79bd3c5cc8b
Seb <git@erhardt.net>
2022-03-16 09:54:32 +0100

    doc

--- pkg/identity/service.go
+++ pkg/identity/service.go
@@ -9,14 +9,20 @@ import (
 	"net/mail"
 )
 
+// IdentificationType is the type of the identification
 type IdentificationType string
 
 const (
+	// EmailIdentificationType is the type of identification that is used to identify a user by email
 	EmailIdentificationType IdentificationType = "email"
+	// PhoneIdentificationType is the type of identification that is used to identify a user by phone number
 	PhoneIdentificationType IdentificationType = "phone"
 )
 
+// ResolveRegistrationEmailTemplate resolves the registration email template
 type ResolveRegistrationEmailTemplate func(origin, emailAddress, token string) email.RegistrationEmailTemplate
+
+// ResolvePasswordResetEmailTemplate resolves the password reset email template
 type ResolvePasswordResetEmailTemplate func(origin, emailAddress, token string) email.PasswordResetTemplate
 
 type Service struct {
@@ -48,6 +54,7 @@ func NewService(issuer string, senderEmailAddress mail.Address) *Service {
 	return &s
 }
 
+// SetGraphQLQueryInterface sets the graphql query interface
 func (s *Service) SetGraphQLQueryInterface(rootQueryObject *graphql.Object) *Service {
 	s.gqlRootObject = rootQueryObject
 	// init queries
@@ -57,6 +64,7 @@ func (s *Service) SetGraphQLQueryInterface(rootQueryObject *graphql.Object) *Ser
 	return s
 }
 
+// SetGraphQLMutationInterface sets the graphql mutation interface
 func (s *Service) SetGraphQLMutationInterface(rootMutationObject *graphql.Object) *Service {
 	s.gqlRootMutationObject = rootMutationObject
 	// init mutations
@@ -66,53 +74,66 @@ func (s *Service) SetGraphQLMutationInterface(rootMutationObject *graphql.Object
 	return s
 }
 
+// SetSQLClient sets the sql client
 func (s *Service) SetSQLClient(client *gorm.DB) *Service {
 	s.sqlClient = client
 	return s
 }
 
+// SetAuthConfirmationEndpoint sets the auth confirmation endpoint
 func (s *Service) SetAuthConfirmationEndpoint(authConfirmationEndpoint string) *Service {
 	s.authConfirmationEndpoint = authConfirmationEndpoint
 	return s
 }
 
+// SetRegistrationEmailResolver sets the registration email resolver
 func (s *Service) SetRegistrationEmailResolver(fn ResolveRegistrationEmailTemplate) *Service {
 	s.registrationEmailResolver = fn
 	return s
 }
 
+// GetSQLClient returns the sql client
 func (s *Service) GetSQLClient() *gorm.DB {
 	return s.sqlClient
 }
 
+// GetIssuer returns the issuer
 func (s *Service) GetIssuer() string {
 	return s.Issuer
 }
 
+// GetSenderEmailAddress returns the sender email address
 func (s *Service) GetSenderEmailAddress() mail.Address {
 	return s.senderEmailAddress
 }
 
+// GetAudience returns the audience
 func (s *Service) GetAudience() string {
 	return s.Audience
 }
 
+// SendEmail sends an email
 func (s *Service) SendEmail(senderAddress mail.Address, receiverAddress mail.Address, subject, content string) error {
 	return s.sendEmail(senderAddress, receiverAddress, subject, content)
 }
 
+// SendSMS sends an sms
 func (s *Service) SendSMS(address string, content string) error {
+	// todo
 	panic("implement me")
 }
 
+// GetEmailTemplate returns the email template
 func (s *Service) GetEmailTemplate() email.GlobalTemplate {
 	return s.emailTemplate
 }
 
+// ResolveRegistrationEmailTemplate returns the registration email template
 func (s *Service) ResolveRegistrationEmailTemplate(origin, emailAddress, confirmationUrl string) email.RegistrationEmailTemplate {
 	return s.registrationEmailResolver(origin, emailAddress, confirmationUrl)
 }
 
+// ResolvePasswordResetEmailTemplate returns the password reset email template
 func (s *Service) ResolvePasswordResetEmailTemplate(origin, emailAddress, confirmationUrl string) email.PasswordResetTemplate {
 	return s.passwordResetEmailResolver(origin, emailAddress, confirmationUrl)
 }
