f8948af104951be1f746722e40c919ce29397202
Seb <git@erhardt.net>
2023-04-21 10:09:12 +0200

    test pepper

--- identity_models/identity_test.go
+++ identity_models/identity_test.go
@@ -1,28 +1,39 @@
 package identity_models
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestUser_VerifyPassword(t *testing.T) {
-	/*
-		pw := "Hello World"
+	t.Parallel()
 
-			user := Identity{}
-			user.SetNewPassword(pw)
+	ass := assert.New(t)
+	testPepper := "THIS_IS_A_TEST_PEPPER_12345"
+	pw := "Hello World"
 
-			ass := assert.New(t)
-			ass.True(user.VerifyPassword(pw))
-			ass.True(user.VerifyPassword(string([]byte(pw))))
-			ass.False(user.VerifyPassword("stupidWrongPassword"))
+	user := Identity{}
+	err := user.SetNewPassword(testPepper, pw)
+	ass.NoError(err)
+	if err != nil {
+		t.Error(err)
+	}
 
-			user.SetNewPassword("pw2")
+	ass.True(user.CheckPassword(testPepper, pw))
+	ass.True(user.CheckPassword(testPepper, string([]byte(pw))))
+	ass.False(user.CheckPassword(testPepper, "stupidWrongPassword"))
 
-			ass.False(user.VerifyPassword(pw))
-			ass.False(user.VerifyPassword(string([]byte(pw))))
-			ass.False(user.VerifyPassword("stupidWrongPassword"))
-			ass.True(user.VerifyPassword("pw2"))
+	err = user.SetNewPassword(testPepper, "pw2")
+	ass.NoError(err)
+	if err != nil {
+		t.Error(err)
+	}
 
-	*/
+	ass.False(user.CheckPassword(testPepper, pw))
+	ass.False(user.CheckPassword(testPepper, string([]byte(pw))))
+	ass.False(user.CheckPassword(testPepper, "stupidWrongPassword"))
+	ass.True(user.CheckPassword(testPepper, "pw2"))
 
+	otherTestPepper := "THIS_IS_A_DIFFERENT_TEST_PEPPER_12345"
+	ass.False(user.CheckPassword(otherTestPepper, "pw2"))
 }
