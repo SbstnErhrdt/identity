b87b9bb5fd9b1f6b490cf0b163ae2428887988b6
Seb <git@erhardt.net>
2023-08-23 14:44:57 +0200

    add admin

--- identity_install/install.go
+++ identity_install/install.go
@@ -24,6 +24,7 @@ func Install(db *gorm.DB) (err error) {
 		identity_models.IdentityRegistrationConfirmation{},
 		identity_models.IdentityResetPassword{},
 		identity_models.IdentityApiToken{},
+		identity_models.IdentityAdmin{},
 	)
 	if err != nil {
 		slog.With("err", err).Error("failed to create / migrate database")
